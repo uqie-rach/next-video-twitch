46c5f04b1dc0ec6db076a8373bb8dce852bb5127
Uqie Rach <achmadfurqonrachmadie@gmail.com>
2025-03-22 10:47:49 +0700

    add shadcn ui components

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,7 +20,8 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
-        custom: "text-white bg-blue-500"
+        custom: "text-white bg-blue-500",
+        primary: 'text-primary bg-blue-600 hover:bg-blue-600/80'
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
