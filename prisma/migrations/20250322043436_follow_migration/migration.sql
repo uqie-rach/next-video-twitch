6de9ec17278cf8be81ba485576afd65b36f0a89f
Uqie Rach <achmadfurqonrachmadie@gmail.com>
2025-03-23 08:01:11 +0700

    add follow migration

---
+++ prisma/migrations/20250322043436_follow_migration/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Follow" (
+    "id" TEXT NOT NULL,
+    "followerId" TEXT NOT NULL,
+    "followingId" TEXT NOT NULL,
+
+    CONSTRAINT "Follow_pkey" PRIMARY KEY ("id")
+);
