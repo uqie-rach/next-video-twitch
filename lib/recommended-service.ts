e2f848e7cd8ab1d9afd06ca77340b973ccdc2f51
Uqie Rach <achmadfurqonrachmadie@gmail.com>
2025-03-27 09:23:39 +0700

    Fixed security issue on stream data

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -43,7 +43,11 @@ export const getRecommended = async () => {
         ]
       },
       include: {
-        stream: true
+        stream: {
+          select: {
+            isLive: true
+          }
+        }
       },
       orderBy: {
         createdAt: 'desc'
@@ -55,7 +59,11 @@ export const getRecommended = async () => {
         createdAt: 'desc'
       },
       include: {
-        stream: true
+        stream: {
+          select: {
+            isLive: true
+          }
+        }
       },
     });
   }
